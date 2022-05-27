9b74aa6b8d033aaa0774767a363feffae8a9e34b
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-05-27 16:50:16 -0300

    Conclusão dos testes

--- tests/pages/login.spec.js
+++ tests/pages/login.spec.js
@@ -9,13 +9,12 @@ jest.mock("../../src/lib/export.js");
 jest.mock("../../src/lib/authentication.js");
 
 describe("pagina de login", () => {
-  it("deverá logar usuário com sucesso", () => {
+  it("Should login the user successfully", () => {
     userLogin.mockResolvedValueOnce();
 
     const page = login();
     const user = page.querySelector(".email");
     const password = page.querySelector(".password");
-    const loginBtn = page.querySelector(".enter");
 
     user.value = "teste@lab.com";
     password.value = "1234567";
@@ -25,3 +24,27 @@ describe("pagina de login", () => {
 });
 
 const error = { code: "udh/internal-error" };
+
+it("should return an error message if the email or password is invalid", () => {
+  userLogin.mockResolvedValueOnce(error);
+  const page = login();
+  const user = page.querySelector(".email");
+  const password = page.querySelector(".password");
+
+  user.value = "teste@lab.com";
+  password.value = "1234567";
+  page.submit();
+
+  expect(userLogin).toHaveBeenCalledWith("teste@lab.com", "1234567");
+});
+
+describe(userGoogle, () => {
+  it("if the user is valid, he/she must log in with the Google account", () => {
+    userGoogle.mockResolvedValueOnce();
+    const page = login();
+    const btnGoogle = page.querySelector(".google");
+
+    btnGoogle.dispatchEvent(new Event("click"));
+    expect(userGoogle).toHaveBeenCalledTimes(1);
+  });
+});
