03a1e8267bd957d01254d368b6efb3cba7a05cce
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-07-28 09:35:53 -0300

    ajustes botão sair

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -12,14 +12,14 @@ export default () => {
   const container = document.createElement("section");
   const template = `
     <section id="post" class="post">
-      <div class= "title-feed">
-        <img src="./images/title.png" alt="leiturama" class= "title-image-feed">
-      </div>
-      <button id= "logout" class= "logout"><svg width="37" height="37" stroke-width="2.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 12H19M19 12L16 15M19 12L16 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M19 6V5C19 3.89543 18.1046 3 17 3H7C5.89543 3 5 3.89543 5 5V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-      </button>
+        <div class= "title-feed">
+          <img src="./images/title.png" alt="leiturama" class= "title-image-feed">
+        </div>
+        <button id= "logout" class= "logout"><svg width="37" height="37" stroke-width="2.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 12H19M19 12L16 15M19 12L16 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M19 6V5C19 3.89543 18.1046 3 17 3H7C5.89543 3 5 3.89543 5 5V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </button>
       <div class="container-template">
         <p class="tip">Sua dica de leitura:
           <textarea id="post-text" class="post-text" rows="5" cols="55" maxlength="180" placeholder="Escreva aqui"></textarea>
